219c69955996d7378f82785e821d34d71bbf1ea8
itachyutiwa <devmail.itachyutiwa@gmail.com>
2021-05-22 08:40:40 +0100

    Initial commit

---
+++ src/scala/scala/SparkApp.scala
@@ -0,0 +1,32 @@
+import org.apache.spark.sql._
+import org.apache.spark._
+object SparkApp {
+
+  def sessionSpark(): Unit ={
+    System.setProperty("hadoop.home.dir", "C:\\Hadoop")
+    val ss =SparkSession.builder()
+      .master(master = "local[*]")
+      .appName(name = "MonPremierProject")
+      .getOrCreate()
+
+    val rdd1 = ss.sparkContext.parallelize(Seq("Its my first scala application. Im a student of INPHB  "))
+    rdd1.foreach(f => println(f))
+    val df_1 = ss.read
+      .format(source="csv")
+      .option("inferSchema", "true")
+      .option("delimiter",";")
+      .option("header","true")
+      .csv(path = "C:\\Users\\akone\\IdeaProjects\\MonPremierProjet\\orders.csv")
+
+    df_1.show(numRows = 10)
+    df_1.printSchema()
+    df_1.createOrReplaceTempView(viewName = "orders")
+    ss.sql(sqlText = "SELECT * FROM orders WHERE city = 'NEWTON' ").show()
+
+  }
+
+  def main(args: Array[String]): Unit ={
+    sessionSpark()
+  }
+
+}
